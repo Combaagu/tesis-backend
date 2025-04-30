44d9060f9bfcd472a6973daf42e8cf4b03622249
Agustin comba <combaagu@gmail.com>
2025-04-30 17:46:49 -0300

    add .env, .env.Api

--- apiGateway/index.js
+++ apiGateway/index.js
@@ -1,40 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import jwksRsa from 'jwks-rsa';
-import { expressjwt } from 'express-jwt';
+import checkJwt from './middlewares/checkJwt.js';
 
-dotenv.config(); // Carga el .env local
+dotenv.config({ path: './apiGateway/.env.apiGateway' });
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORTAPI;
 
-// Middleware para parsear JSON
 app.use(express.json());
 
-// Middleware JWT con Auth0
-const checkJwt = expressjwt({
-  secret: jwksRsa.expressJwtSecret({
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-  }),
-  audience: process.env.AUTH0_CLIENT_ID,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256'],
-});
-
-// Ruta pública
 app.get('/', (req, res) => {
-  res.send('¡API Gateway en funcionamiento!');
+  res.send('¡API Gateway en funcionamiento weeeeeeeb!');
 });
 
-// Ruta protegida
 app.get('/protegida', checkJwt, (req, res) => {
-  res.send('¡Ruta protegida! El token es válido.');
+  res.send('¡Ruta protegida! Token válido.');
 });
 
-// Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Servidor APIgateway - corriendo en el puerto ${port}`);
+  console.log(`Servidor API Gateway corriendo en el puerto ${port}`);
 });
